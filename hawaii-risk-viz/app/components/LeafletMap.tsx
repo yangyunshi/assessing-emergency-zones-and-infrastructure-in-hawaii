9a0b8fe9db3d883b7741657930bf2ff6f4af868e
yangyunshi <113159604+yangyunshi@users.noreply.github.com>
2025-12-10 00:01:18 -1000

    Update LeafletMap.tsx
    changed map to carto-positron

--- hawaii-risk-viz/app/components/LeafletMap.tsx
+++ hawaii-risk-viz/app/components/LeafletMap.tsx
@@ -434,8 +434,9 @@ function LeafletMap({ layers, userLocations = [] }: LeafletMapProps) {
       <ZoomWatcher onZoomChange={setZoom} />
 
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        attribution='&copy; <a href="https://www.carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
       {/* Polygon / line layers */}
